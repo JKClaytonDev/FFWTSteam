c29e348b56975f3ae48171210b15c68c41036c7d
Jackson Kenneth Clayton <59662161+JKClaytonDev@users.noreply.github.com>
2022-04-20 03:04:19 -0400

    first

---
+++ Assets/heavyrecoil.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heavyrecoil : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Application.OpenURL("https://store.steampowered.com/app/933030/Heavy_Recoil/");
+        Application.Quit();
+    }
+}
---
+++ Assets/playerEnableEnemies.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerEnableEnemies : MonoBehaviour
+{
+    public GameObject[] enemies;
+    void Start()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+        foreach (GameObject e in enemies)
+            e.SetActive(false);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<Movement>())
+        {
+            foreach (GameObject e in enemies)
+                e.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
